10ee284ad8201ac13a65e3f0036489c220b0868b
Umitgn <9024902@student.zadkine.nl>
2024-11-22 13:58:25 +0100

    opdr3

---
+++ week2/opdr2/contact.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>contact</title>
+</head>
+<body>
+    <h1>contact</h1>
+<form>
+<label for="name">Naam</label>
+<input type="text" name="name">
+<label for="email">E-mail</label>
+<input type="text" name="E-mail">
+<label for="message">bericht</label>
+<input type="text" name="bericht">
+
+
+
+
+</form>
+
+
+
+
+<?php
+include 'navbar.php';
+include 'footer.php';
+?>
+</body>
+</html>
