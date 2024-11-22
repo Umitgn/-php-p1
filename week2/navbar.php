c7da6f82adcea7091a9feec3ea7e867cfa616e9e
Umitgn <9024902@student.zadkine.nl>
2024-11-22 13:41:40 +0100

    week2

---
+++ week2/navbar.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>navbar</title>
+</head>
+<body>
+    <h1>dit is de navbar</h1>
+<nav>
+    <ul>
+        <li>
+            <a href="index.php">home</a>
+            <a href="about.php">over</a>
+            <a href="contact.php">contact</a>
+        </li>
+    </ul>
+</nav>
+
+
+
+
+
+</body>
+</html>
