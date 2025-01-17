0ec39196dd470bc1753f9150d22625d23fe4c290
Umitgn <9024902@student.zadkine.nl>
2025-01-17 13:45:34 +0100

    opdr9

--- week7/index.php
+++ week7/index.php
@@ -1,23 +1,25 @@
+<?php
+session_start();
+// Start de sessie
+$_SESSION['visits']++;
+$visits = $_SESSION["visits"];
+$allvisits = $_COOKIE["all_visits"] + 1;
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>opdr9</title>
+    <title>week 7</title>
 </head>
 <body>
-<?php 
-session_start();
-$_SESSION['username'] = 'patrick'
-
-$_SESSION['username'] = $_POST ['username']
-?>    
 
 <?php 
+echo "Deze pagina heb je al : $visits keer bekeken voordat je de internet browser hebt afgesloten";
 
 ?>
 
-
-
 </body>
 </html>
+
